0a15f4fd063f46b22a437f26cc19c61d509c37a2
simonTomSt <simontomst.github@gmail.com>
2023-09-28 21:18:33 +0200

    Implement option for adding global params for views

--- Core/View.php
+++ Core/View.php
@@ -4,16 +4,25 @@ namespace App\Core;
 
 class View
 {
+    public static array $globalParams = [];
+
     public static function renderView(string $view, array $additionalData = [], string $layout = null): false|string
     {
         $params = array_map(fn(mixed $param): string => htmlspecialchars((string)$param), $additionalData);
-
+        $globalParams = self::$globalParams;
+        
         $pagePath = Application::resolveFilePath("templates/views/{$view}");
         $bodyPath = $layout ? Application::resolveFilePath("templates/layouts/{$layout}") : $pagePath;
+
         ob_start();
 
         include_once Application::resolveFilePath("templates/layouts/root");
 
         return ob_get_clean();
+    }
+
+    public static function addGlobalParam(string $key, mixed $value): void
+    {
+        self::$globalParams[$key] = $value;
     }
 }
