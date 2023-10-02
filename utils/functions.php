c045e45d38e8bed8370ac9030778a11617310188
simonTomSt <simontomst.github@gmail.com>
2023-10-02 11:18:15 +0200

    Implement function to generate pagination

--- utils/functions.php
+++ utils/functions.php
@@ -30,3 +30,36 @@ function generateInputField($params): void
         echo "<div class=\"invalid-feedback\">$errorMessage</div>";
     }
 }
+
+
+function generatePagination(int $totalCount, string $baseUrl, int $currentOffset, int $limit): void
+{
+    echo '<nav aria-label="Page navigation example"><ul class="pagination">';
+
+    // Calculate the total number of pages
+    $totalPages = ceil($totalCount / $limit);
+
+    // Previous page link
+    if ($currentOffset > 0) {
+        echo '<li class="page-item"><a class="page-link" href="' . $baseUrl . '?offset=' . max(0, $currentOffset - $limit) . '">Previous</a></li>';
+    }
+
+    // Generate page links
+    for ($page = 1; $page <= $totalPages; $page++) {
+        // Show the first 10 pages, then add three dots
+        if ($page <= 10 || ($page >= ($currentOffset / $limit - 1) && $page <= ($currentOffset / $limit + 2)) || $page == $totalPages) {
+            echo '<li class="page-item ' . ($page == ($currentOffset / $limit + 1) ? 'active' : '') . '"><a class="page-link" href="' . $baseUrl . '?offset=' . (($page - 1) * $limit) . '">' . $page . '</a></li>';
+        } elseif ($page == 11) {
+            echo '<li class="page-item disabled"><span class="page-link">...</span></li>';
+        }
+    }
+
+    // Next page link
+    if ($currentOffset < ($totalPages - 1) * $limit) {
+        echo '<li class="page-item"><a class="page-link" href="' . $baseUrl . '?offset=' . ($currentOffset + $limit) . '">Next</a></li>';
+    }
+
+    echo '</ul></nav>';
+}
+
+
